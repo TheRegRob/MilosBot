60fb5851d57fa5d8c5e8403c2a3e5fca2ce7ffd5
Robin <robin.reggiani@gmail.com>
2023-11-24 01:16:58 +0100

    Changed visibility and reset token

--- milosbot/classes/milos.py
+++ milosbot/classes/milos.py
@@ -22,4 +22,4 @@ async def on_message(message):
         await message.channel.send("https://tenor.com/view/flex-air-flex-air5-ricardo-bongo-cat-bathtub-gif-19565157")
 
 
-client.run('OTA4NzI1NjQ0MDM1MjQ0MDMz.GriekE.UjQzUCe2HTMf6Y4uGB-_kINTdtBhIE-6KBtuh0')
+client.run('OTA4NzI1NjQ0MDM1MjQ0MDMz.GuUfB3.nKJxlHz0ihMpdZxliVDouhdkb8h_PEm7d6o9XQ')
